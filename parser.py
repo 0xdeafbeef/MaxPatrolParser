fcafb8c084a8a4cbd865dd7f8c5069a7e0c661cd
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-27 21:33:52 +0300

    :rocket: stable version, need to implemnt menu and cve id

--- parser.py
+++ parser.py
@@ -1,60 +1,57 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import xml.etree.ElementTree as ET
-import xlwt
+import csv
 import sys
 
 namespaces = {'PT': 'http://www.ptsecurity.ru/reports'}
 protocols = {'6': 'TCP', '17': 'UDP'}
+port_status = {'0': 'open', '1': 'locked', '2': 'unavailable'}
 
 
-def mp_parse(filename):
-    with open(filename, 'r') as xml_file:
-        xml_tree = ET.parse(xml_file)
-        xml_root = xml_tree.getroot()
-        hosts_list = []
-        for host in xml_root.findall("./PT:data/PT:host", namespaces):
-            soft_list = []
-            host_dict = {'soft': [], 'ip': host.attrib['ip']}
-            for soft in host.findall('PT:scan_objects/PT:soft', namespaces):
-                vulners_list = []
-                soft_dict = {'vulners': [], 'name': soft.find('PT:name', namespaces).text,
-                             'port': soft.attrib['port'] + '/' + protocols[soft.attrib['protocol']]}
-                for vulner in soft.findall('PT:vulners/PT:vulner', namespaces):
-                    vulner_dict = {}
-                    vulner_id = vulner.attrib['id']
-                    vulner_dict['reliability'] = vulner.attrib['status']
-                    for vulner_descr in xml_root.findall("./PT:vulners/PT:vulner", namespaces):
-                        if vulner_id == vulner_descr.attrib['id']:
-                            if vulner_descr.find('PT:cvss', namespaces) is not None and \
-                                    vulner_descr.find('PT:cvss', namespaces).attrib['base_score'] != '0.0':
-                                vulner_dict['id'] = vulner_id
-                                vulner_dict['title'] = vulner_descr.find('PT:title', namespaces).text
-                                vulner_dict['base_score'] = vulner_descr.find('PT:cvss', namespaces).attrib[
-                                    'base_score']
-                                vulner_dict['base_score_decomp'] = vulner_descr.find('PT:cvss', namespaces).attrib[
-                                    'base_score_decomp']
-                                vulner_dict['description'] = vulner_descr.find('PT:description', namespaces).text
-                                vulner_dict['short_description'] = vulner_descr.find('PT:short_description',
-                                                                                     namespaces).text
-                                vulner_dict['how_to_fix'] = vulner_descr.find('PT:how_to_fix', namespaces).text
-                                try:
-                                    vulner_dict['cve'] = \
-                                        vulner_descr.find('PT:global_id[@name="CVE"]', namespaces).attrib['value']
-                                except:
-                                    vulner_dict['cve'] = 'None'
-                                try:
-                                    vulner_dict['links'] = vulner_descr.find('PT:links', namespaces).text
-                                except:
-                                    vulner_dict['links'] = 'None'
-                                vulners_list.append(vulner_dict)
-                if len(vulners_list):
-                    soft_dict['vulners'] = sorted(vulners_list, key=lambda x: float(x['base_score']), reverse=True)
-                    soft_list.append(soft_dict)
-            if len(soft_list):
-                host_dict['soft'] = soft_list
-                hosts_list.append(host_dict)
-    return hosts_list
+def mp_parse(filename, resname):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    res_data = open(resname, 'w')
+    cw = csv.writer(res_data)
+    csv_head = []
+    count = 0
+    fd = open(resname, 'a')
+    host_info = []
+    for host in root.findall('./PT:data/PT:host', namespaces):
+        appended_info = list()
+        ip = host.attrib['ip']
+        for soft in host.findall('PT:scan_objects/PT:soft', namespaces):
+            appended_info.append(ip)
+            try:
+                appended_info.append(soft.find('PT:banner/PT:table/PT:body/PT:row/PT:field', namespaces).text)
+            except:
+                appended_info.append(None)
+            try:
+                appended_info.append(soft.attrib['port'])
+            except:
+                appended_info.append(None)
+            try:
+                appended_info.append(protocols[soft.attrib['protocol']])
+            except:
+                appended_info.append(None)
+            try:
+                appended_info.append(port_status[soft.attrib['port_status']])
+            except:
+                appended_info.append(None)
+            for vulnerabilty in soft.findall('PT:vulners/PT:vulner', namespaces):
+                for vuln in root.findall('./PT:vulners/vulner', namespaces):
+                    if vuln.attrib['id'] == vulnerabilty.attrib['id']:
+                        try:
+                            appended_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'] + 'cvss')
+                        except:
+                            appended_info.append(None)
+            host_info.append(appended_info)
+    return host_info
 
 
 if __name__ == '__main__':
-    mp_parse('test.xml')
+    res = mp_parse('test.xml', 'kek')
+    print(res)
+# with open('test.csv', 'w', newline='') as file:
+#     wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+#     wr.writerow(res)
