ff034daa6cd181a43f16bbfe841af818454e46cf
0xd34b33f <0xd34b33f@users.noreply.github.com>
2019-05-28 13:34:27 +0300

    :sparkles: First stable realese, doesn't work with any input
    normaly parses xml to csv

--- parser.py
+++ parser.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 import xml.etree.ElementTree as ET
 import csv
-import sys
 
 namespaces = {'PT': 'http://www.ptsecurity.ru/reports'}
 protocols = {'6': 'TCP', '17': 'UDP'}
 port_status = {'0': 'open', '1': 'locked', '2': 'unavailable'}
 
 
-def mp_parse(filename, resname):
+def mp_parse(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
-    res_data = open(resname, 'w')
-    cw = csv.writer(res_data)
-    csv_head = []
-    count = 0
-    fd = open(resname, 'a')
     host_info = []
     for host in root.findall('./PT:data/PT:host', namespaces):
-        appended_info = list()
         ip = host.attrib['ip']
         for soft in host.findall('PT:scan_objects/PT:soft', namespaces):
+            appended_info = list()
             appended_info.append(ip)
+            # finds soft name
             try:
                 appended_info.append(soft.find('PT:banner/PT:table/PT:body/PT:row/PT:field', namespaces).text)
             except:
@@ -38,20 +33,32 @@ def mp_parse(filename, resname):
                 appended_info.append(port_status[soft.attrib['port_status']])
             except:
                 appended_info.append(None)
-            for vulnerabilty in soft.findall('PT:vulners/PT:vulner', namespaces):
-                for vuln in root.findall('./PT:vulners/vulner', namespaces):
-                    if vuln.attrib['id'] == vulnerabilty.attrib['id']:
-                        try:
-                            appended_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'] + 'cvss')
-                        except:
-                            appended_info.append(None)
+            # finds cve and cvss if exists, else sets None
+            vuln_finder(appended_info, root, soft)
             host_info.append(appended_info)
     return host_info
 
 
+def vuln_finder(appended_info, root, soft):
+    for vulnerabilty in soft.findall('PT:vulners/PT:vulner', namespaces):
+        for vuln in root.findall('./PT:vulners/PT:vulner', namespaces):
+            if vuln.attrib['id'] == vulnerabilty.attrib['id']:
+                try:
+                    appended_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'])
+                except:
+                    appended_info.append(None)
+                try:
+                    appended_info.append(vuln.find('PT:global_id', namespaces).attrib['value'])
+                except:
+                    appended_info.append(None)
+                try:
+                    appended_info.append(vuln.find('PT:description', namespaces).text)
+                except:
+                    appended_info.append(None)
+
+
 if __name__ == '__main__':
-    res = mp_parse('test.xml', 'kek')
-    print(res)
-# with open('test.csv', 'w', newline='') as file:
-#     wr = csv.writer(file, quoting=csv.QUOTE_ALL)
-#     wr.writerow(res)
+    res = mp_parse('test.xml')
+    with open('test.csv', 'w', newline='') as file:
+        wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+        wr.writerows(res)
