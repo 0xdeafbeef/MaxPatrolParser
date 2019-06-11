1fcbf5aa22629a34a5e1fbc53ddddaf687cca4a5
0x34d33f <0x34db33f@noreply.github.com>
2019-06-11 15:25:58 +0300

    Added caching of vulnerabilities. Speed improvements. Big memory consumption on really big xmls(800mb - 6gb of memory) because of strange mp data structure.

--- parser.py
+++ parser.py
@@ -2,6 +2,8 @@
 import xml.etree.ElementTree as ET
 import csv
 import argparse as arp
+import gc
+import sys
 
 namespaces = {'PT': 'http://www.ptsecurity.ru/reports'}
 protocols = {'6': 'TCP', '17': 'UDP'}
@@ -12,16 +14,28 @@ def mp_parse(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     host_info = []
+    appended_info = ['ip', 'fqdn', 'os', 'soft_name', 'port', 'protocol', 'port status', 'Vuln id', 'CVSS', 'CVE',
+                     'description', 'start time', 'stop_time']
+    host_info.append(appended_info)
+    vuln_table_creator(root)
     for host in root.findall('./PT:data/PT:host', namespaces):
         ip = host.attrib['ip']
+        fqdn = host.attrib['fqdn']
+        start_time = host.attrib['start_time']
+        stop_time = host.attrib['stop_time']
+        os = get_os_info(host)
         for soft in host.findall('PT:scan_objects/PT:soft', namespaces):
             appended_info = list()
             appended_info.append(ip)
+            appended_info.append(fqdn)
+            appended_info.append(os)
+            appended_info.append(soft.find('PT:name', namespaces).text)
             # finds soft name
-            try:
-                appended_info.append(soft.find('PT:banner/PT:table/PT:body/PT:row/PT:field', namespaces).text)
-            except:
-                appended_info.append(None)
+            # todo check this
+            # try:
+            #     appended_info.append(soft.find('PT:banner/PT:table/PT:body/PT:row/PT:field', namespaces).text)
+            # except:
+            #     appended_info.append(None)
             try:
                 appended_info.append(soft.attrib['port'])
             except:
@@ -34,31 +48,52 @@ def mp_parse(filename):
                 appended_info.append(port_status[soft.attrib['port_status']])
             except:
                 appended_info.append(None)
+
             # finds cve and cvss if exists, else sets None
-            vuln_finder(appended_info, root, soft, host_info)
+            vuln_finder(appended_info, soft, host_info, start_time, stop_time)
     return host_info
 
 
-def vuln_finder(appended_info, root, soft, host_info):
-    appended_info_copy = appended_info.copy()
+def get_os_info(host):
+    for prod_type in host.findall('PT:scan_objects/PT:soft', namespaces):
+        if prod_type.attrib['type'] == '2':
+            os = prod_type.find('PT:name', namespaces).text
+            break
+    try:
+        os
+    except NameError:
+        os = None
+    return os
+
+
+def vuln_finder(appended_info, soft, host_info, start_time, stop_time):
+    # appended_info_copy = appended_info.copy()
     for vulnerabilty in soft.findall('PT:vulners/PT:vulner', namespaces):
-        for vuln in root.findall('./PT:vulners/PT:vulner', namespaces):
-            if vuln.attrib['id'] == vulnerabilty.attrib['id']:
-                try:
-                    appended_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'])
-                except:
-                    appended_info.append(None)
-                try:
-                    appended_info.append(vuln.find('PT:global_id', namespaces).attrib['value'])
-                except:
-                    appended_info.append(None)
-                try:
-                    appended_info.append(vuln.find('PT:description', namespaces).text)
-                except:
-                    appended_info.append(None)
-                host_info.append(appended_info)
-                appended_info = appended_info_copy.copy()
-                break
+        host_info.append(
+            appended_info + [vulnerabilty.attrib['id']] + vulners_fast_table[vulnerabilty.attrib['id']] +
+            [start_time, stop_time])
+        break
+
+
+vulners_fast_table = dict()
+
+
+def vuln_table_creator(root):
+    for vuln in root.findall('./PT:vulners/PT:vulner', namespaces):
+        vuln_info = list()
+        try:
+            vuln_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'])
+        except:
+            vuln_info.append(None)
+        try:
+            vuln_info.append(vuln.find('PT:global_id', namespaces).attrib['value'])
+        except:
+            vuln_info.append(None)
+        try:
+            vuln_info.append(vuln.find('PT:description', namespaces).text)
+        except:
+            vuln_info.append(None)
+        vulners_fast_table.update({vuln.attrib['id']: vuln_info})
 
 
 if __name__ == '__main__':
@@ -77,6 +112,8 @@ if __name__ == '__main__':
     else:
         output_path = args.output
     res = mp_parse(input_path)
-    with open(output_path, 'w', newline='') as file:
+    print(sys.getsizeof(vulners_fast_table))
+    with open(output_path, 'a+', newline='') as file:
+        file.write('sep=,\r\n')
         wr = csv.writer(file, quoting=csv.QUOTE_ALL, dialect='excel')
         wr.writerows(res)
