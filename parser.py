69c5af48f91d5848f19ed3dc2ec469de129317fc
0x34d33f <0x34db33f@noreply.github.com>
2019-06-19 14:19:22 +0300

    itterative parsing rework

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
-import xml.etree.ElementTree as ET
 import csv
+import lxml
 import argparse as arp
 import sys
 from excel_saver import save_to_excell
@@ -33,73 +33,6 @@ def risk_level(cvss, reliability):
             return 'Low (Suspicious)'
 
 
-def mp_parse(filename, output_file, flags):
-    level = flags.level
-    cve_is_needed = flags.cve
-    tree = ET.parse(filename)
-    root = tree.getroot()
-    host_info = []
-    appended_info = ['ip', 'fqdn', 'os', 'soft name', 'soft version', 'soft path', 'port', 'protocol', 'port status',
-                     'Patrol vulner id', 'Vulner name', 'CVSS', 'CVE', 'Vulnerability rate', 'Patrol vulnerability rate'
-                                                                                             'description',
-                     'how to fix', 'links', 'start time', 'stop_time']
-    host_info.append(appended_info)
-    vuln_table_creator(root)
-    cwr = csv.writer(output_file, quoting=csv.QUOTE_ALL, dialect='excel')
-    for host in root.findall('./PT:data/PT:host', namespaces):
-        ip = host.attrib['ip']
-        fqdn = host.attrib['fqdn']
-        start_time = host.attrib['start_time']
-        stop_time = host.attrib['stop_time']
-        os = get_os_info(host)
-        for soft in host.findall('PT:scan_objects/PT:soft', namespaces):
-            appended_info = list()
-            appended_info.append(ip)
-            appended_info.append(fqdn)
-            appended_info.append(os)
-            appended_info.append(soft.find('PT:name', namespaces).text)
-            try:
-                appended_info.append(soft.find('PT:version', namespaces).text)
-            except:
-                appended_info.append(None)
-            try:
-                appended_info.append(soft.find('PT:path', namespaces).text)
-            except:
-                appended_info.append(None)
-            try:
-                appended_info.append(soft.attrib['port'])
-            except:
-                appended_info.append(None)
-            try:
-                appended_info.append(protocols[soft.attrib['protocol']])
-            except:
-                appended_info.append(None)
-            try:
-                appended_info.append(port_status[soft.attrib['port_status']])
-            except:
-                appended_info.append(None)
-            # finds cve and cvss if exists, else sets None
-            if vuln_finder(appended_info, soft, host_info, start_time, stop_time,
-                           level, cve_is_needed) == 0 and level == 0:
-                appended_info += ([None] * 7 + [start_time, stop_time])
-                host_info.append(appended_info)
-        cwr.writerows(host_info)
-        host_info = []
-
-
-def get_os_info(host):
-    for prod_type in host.findall('PT:scan_objects/PT:soft', namespaces):
-        if prod_type.attrib['type'] == '2':
-            os_name = prod_type.find('PT:name', namespaces).text
-            os_name = os_name + ' ' + prod_type.find('PT:version', namespaces).text
-            break
-    try:
-        os_name
-    except NameError:
-        os_name = None
-    return os_name
-
-
 def patrol_level(lvl: str):
     return \
         {'0': 'доступна информация',
@@ -110,97 +43,67 @@ def patrol_level(lvl: str):
          '5': 'высокий уровень'}[lvl]
 
 
-def vuln_finder(appended_info: list, soft: ET.Element, host_info, start_time: str, stop_time: str, level: list,
-                cve: bool):
+def fast_iter(context, func, *args, **kwargs):
+    """
+    http://lxml.de/parsing.html#modifying-the-tree
+    Based on Liza Daly's fast_iter
+    http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
+    See also http://effbot.org/zone/element-iterparse.htm
+    """
+    for event, elem in context:
+        func(elem, *args, **kwargs)
+        # It's safe to call clear() here because no descendants will be
+        # accessed
+        elem.clear()
+        # Also eliminate now-empty references from the root node to elem
+        for ancestor in elem.xpath('ancestor-or-self::*'):
+            while ancestor.getprevious() is not None:
+                del ancestor.getparent()[0]
+    del context
+
+
+def vuln_finder():
     counter = 0
-    for vulnerabilty in soft.findall('PT:vulners/PT:vulner', namespaces):
-        if level is None:
-            pass
-        elif int(vulnerabilty.attrib['level']) not in level:
-            continue
-        counter += 1
-        vulners_part = vulners_fast_table[vulnerabilty.attrib['id']]
-        if cve and vulners_part[2] is None:
-            continue
-        try:
-            risk = [risk_level(vulners_part[1], vulnerabilty.attrib['status'])]
-        except:
-            risk = ['Info']
-        patrol_risk = [patrol_level(vulnerabilty.attrib['level'])]
-        risk.append(patrol_risk)
-        host_info.append(
-            appended_info + [vulnerabilty.attrib['id']] + vulners_part[:3] + risk + vulners_part[3:] +
-            [start_time, stop_time])
-    return counter
+    context = lxml.etree.iterparse('отчет Сервер БД Audit XML_all_part_2.xml', tag='schedule', events=('end',))
+    fast_iter(context, lambda x: print(x))
 
 
-vulners_fast_table = dict()
+# if __name__ == '__main__':
+#     parser = arp.ArgumentParser(prog='MaxPatrolParser   ')
+#     parser.add_argument('-p', '--input-path', help='Path to xml file')
+#     parser.add_argument('-o', '--output', help='Path to output file')
+#     parser.add_argument('-l', '--level', nargs='+',
+#                         help='Level of vulnerability. Like -l 1 2 4\n'
+#                              ' 0 - info\n'
+#                              ' 1 - low\n'
+#                              ' 2 - medium (suspicious)\n'
+#                              ' 3 - medium\n'
+#                              ' 4 - high (suspicious)\n'
+#                              ' 5 - high\n')
+#     parser.add_argument('-e', '--excel', action='store_true', help='Output into xlsx file')
+#     parser.add_argument('--cve', action='store_true', help='Saves rows in which cve is presented')
+#     args = parser.parse_args()
+#     if args.input_path is None:
+#         parser.print_help()
+#         print("[-] -p target parameter required")
+#         exit(1)
+#     input_path = args.input_path
+#     if args.level is not None:
+#         args.level = list(map(lambda x: int(x), args.level))
+#     if args.output is None:
+#         output_path = 'output.csv'
+#     else:
+#         output_path = args.output
+#     try:
+#         os.remove(output_path)
+#     except FileNotFoundError:
+#         pass
+#     output_csv_file = open(output_path, 'a+', newline='')
+#     # mp_parse(input_path, output_csv_file, args)
+#     output_csv_file.seek(0)
+#     if args.excel:
+#         save_to_excell(output_path)
+#
+#     sys.exit(0)
 
-
-def vuln_table_creator(root: ET, ):
-    for vuln in root.findall('./PT:vulners/PT:vulner', namespaces):
-        vuln_info = list()
-        try:
-            vuln_info.append(vuln.find('PT:title', namespaces).text)
-        except:
-            vuln_info.append(None)
-        try:
-            vuln_info.append(vuln.find('PT:cvss', namespaces).attrib['base_score'])
-        except:
-            vuln_info.append(None)
-        try:
-            vuln_info.append(vuln.find('PT:global_id', namespaces).attrib['value'])
-        except:
-            vuln_info.append(None)
-        try:
-            vuln_info.append(vuln.find('PT:description', namespaces).text)
-        except:
-            vuln_info.append(None)
-        try:
-            vuln_info.append(vuln.find('PT:how_to_fix', namespaces).text)
-        except:
-            vuln_info.append(None)
-        try:
-            vuln_info.append(vuln.find('PT:links', namespaces).text)
-        except:
-            vuln_info.append(None)
-        vulners_fast_table.update({vuln.attrib['id']: vuln_info})
-
-
-if __name__ == '__main__':
-    parser = arp.ArgumentParser(prog='MaxPatrolParser   ')
-    parser.add_argument('-p', '--input-path', help='Path to xml file')
-    parser.add_argument('-o', '--output', help='Path to output file')
-    parser.add_argument('-l', '--level', nargs='+',
-                        help='Level of vulnerability. Like -l 1 2 4\n'
-                             ' 0 - info\n'
-                             ' 1 - low\n'
-                             ' 2 - medium (suspicious)\n'
-                             ' 3 - medium\n'
-                             ' 4 - high (suspicious)\n'
-                             ' 5 - high\n')
-    parser.add_argument('-e', '--excel', action='store_true', help='Output into xlsx file')
-    parser.add_argument('--cve', action='store_true', help='Saves rows in which cve is presented')
-    args = parser.parse_args()
-    if args.input_path is None:
-        parser.print_help()
-        print("[-] -p target parameter required")
-        exit(1)
-    input_path = args.input_path
-    if args.level is not None:
-        args.level = list(map(lambda x: int(x), args.level))
-    if args.output is None:
-        output_path = 'output.csv'
-    else:
-        output_path = args.output
-    try:
-        os.remove(output_path)
-    except FileNotFoundError:
-        pass
-    output_csv_file = open(output_path, 'a+', newline='')
-    mp_parse(input_path, output_csv_file, args)
-    output_csv_file.seek(0)
-    if args.excel:
-        save_to_excell(output_path)
-
-    sys.exit(0)
+vuln_finder()
